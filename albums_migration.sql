1c5c6a080aa139458dc3553c9a20b27c223ddde1
AaronBoehle <boehle.aaron@gmail.com>
2019-01-28 16:10:13 -0600

    mysql select exercise complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,11 @@ USE codeup_test_db;
 
 create table albums (
   id int unsigned not null auto_increment,
-  artist varchar (50),
-  name varchar (50),
-  release_date date,
-  sales int unsigned,
-  genre varchar (50),
+  artist varchar (75),
+  name varchar (75),
+  release_date int,
+  sales YEAR unsigned,
+  genre varchar (75),
   primary key (id)
 );
+
---
+++ select_exercises.sql
@@ -0,0 +1,20 @@
+use codeup_test_db;
+
+Select 'All Pink Floyd albums:' AS 'Info';
+select * from albums where artist = 'pink floyd';
+
+Select 'The year Sgt. Pepper''s Lonely Hearts Club' AS 'Info';
+select release_date from albums where name like '%sgt. pepper%';
+
+
+Select 'The genre of Nevermind:' AS 'Info';
+select genre from albums where name = 'nevermind';
+
+select 'Albums released in the 90''s' as 'Info';
+select * from albums where release_date between 1990 and 1999;
+
+select 'Albums with less than 20 million certified sales' as 'Info';
+select * from albums where sales <20 \G
+
+select 'Anything rock!' as 'Info';
+select * from albums where genre like '%rock%' \G
